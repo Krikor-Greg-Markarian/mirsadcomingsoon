3d60ff62f363b828765aaa382f5dc2dc63ccd099
Greg <greg_markarian@hotmail.com>
2022-01-10 10:13:03 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import cn from 'classnames'
+export default function Home() {
+  return (
+    <div>
+        <h1 className={cn("font-bold")}>asdsadsa</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightpurple: "#6C63FF",
+        white: "#FFFFFF",
+        black: "#110D28",
+        lightgreen: "#44FBBC",
+      },
+    },
+  },
+  plugins: [],
+};
